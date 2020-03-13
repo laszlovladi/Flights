0bef22de486a4bf7f3055bade090d6e5f996389b
ondrejtrisc <57498031+ondrejtrisc@users.noreply.github.com>
2020-03-13 09:52:04 +0100

    calling the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import { DateTime } from 'luxon';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Start Here
-      </div>
-    );
-  }
+const App = () => {
+
+  const when = DateTime.local().plus({days: 1}).toFormat('dd/MM/yyyy');
+  const query = new URLSearchParams({
+    partner: 'picky',
+    flyFrom: 'PRG',
+    to: 'VLC'
+  })
+  const url = new URL(`?${query}`, 'https://api.skypicker.com/flights');
+
+  const getSearchResults = async () => {
+    
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+  };
+
+  getSearchResults();  
+  
+  return (
+    <div className="App">
+      test
+    </div>
+  );
 }
+
 
 export default App;
